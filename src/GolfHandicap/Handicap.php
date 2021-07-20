92f627f7fe5eb20b8cbfebba71c750f0e14f8787
Michael Schmidt <mschmidt74@hotmail.com>
2021-07-20 14:06:57 -0400

    Initial commit

---
+++ src/GolfHandicap/Handicap.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace GolfHandicap;
+
+class Handicap
+{
+    private $roundsEnteredArray;
+    private $scores;
+    private $numberOfScores;
+
+    /**
+     * Handicap constructor.
+     * @param array $scores
+     */
+    public function __construct(array $scores)
+    {
+        $this->roundsEnteredArray = [0,1,1,1,1,1,1,2,2,3,3,4,4,5,5,6,6,7,8,9,10];
+        $this->scores = $scores; // These should be ESC adjusted scores
+        $this->numberOfScores = count($this->scores);
+    }
+
+    /**
+     * @return array
+     */
+    public function getScores()
+    {
+        return $this->scores;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getDifferential()
+    {
+        return $this->roundsEnteredArray[$this->numberOfScores];
+    }
+
+    /**
+     * @return float
+     */
+    public function getHandicap()
+    {
+        return ((array_sum($this->scores)/$this->numberOfScores) * 0.96) - 72;
+    }
+}
---
+++ tests/HandicapTest.php
@@ -0,0 +1,35 @@
+<?php
+
+use GolfHandicap\Handicap;
+use PHPUnit\Framework\TestCase;
+
+class HandicapTest extends TestCase
+{
+
+    public function testGetScores()
+    {
+        $scores = [75, 83, 105];
+        $handicap = new Handicap($scores);
+
+        $this->assertEquals($scores, $handicap->getScores());
+    }
+
+    public function testGetDifferential()
+    {
+        $scores = [75, 83, 105, 85, 95, 83];
+        $handicap = new Handicap($scores);
+
+        $this->assertEquals(1,$handicap->getDifferential());
+
+        array_push($scores,88);
+        $handicap = new Handicap($scores);
+        $this->assertEquals(2,$handicap->getDifferential());
+    }
+
+    public function testGetHandicap()
+    {
+        $scores = [75, 83, 105, 85, 95, 83];
+        $handicap = new Handicap($scores);
+        $this->assertEquals(12.16, $handicap->getHandicap());
+    }
+}
